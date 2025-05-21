eb6f6047857c5262681c958f2ce5177d55163352
Mike Dettmer <mike@mikedettmer.com>
2025-05-20 23:22:30 -0400

    adds event

--- events.js
+++ events.js
@@ -1,14 +1,29 @@
 const cronitor = require('cronitor')('77d933c4061d44f1b2f9ae46e6860f6c');
+const cron = require('node-cron');
+
+
+(async () => {
+    const monitor = await cronitor.Monitor.put({
+        type: 'job',
+        key: 'events job',
+        schedule: '*/5 * * * *'
+    });
+})();
+
 
 const monitor = new cronitor.Monitor('events job');
-console.log('running background job with monitoring - events!');
 
-monitor.ping({state: 'run'});
+cron.schedule('*/5 * * * *', () => {
+    console.log('running background job with monitoring - events!');
 
-if(Math.random() < 0.33) {
-    monitor.ping({state: 'fail'});
-    throw new Error('random error');
-} else {
-    monitor.ping({state: 'complete'});
-    return;
-}
+    monitor.ping({state: 'run'});
+
+    if(Math.random() < 0.5) {
+        monitor.ping({state: 'fail'});
+        throw new Error('random error');
+    } else {
+        monitor.ping({state: 'complete'});
+        return;
+    }
+});
+
