8b03e64cff1d8f4db2fc55111d110635b2f674a0
Mike Dettmer <mike@mikedettmer.com>
2025-05-20 22:02:48 -0400

    removes iife

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const cronitor = require('cronitor')('77d933c4061d44f1b2f9ae46e6860f6c');
 
 // create an empty IIFE function
-(async () => {
-    // create a monitor
-    const monitor = await cronitor.Monitor.put({
-        type: 'job',
-        key: 'important-background-job',
-        schedule: '59 * * * *'
-    });
 
-    cronitor.wrap('important-background-job', function () {
-        console.log('running background job with monitoring!');
-        
-        if(Math.random() > 0.5) {
-            throw new Error('random error');
-        } else {
-            return;
-        }
-    });
-})();
+cronitor.wrap('important-background-job', function () {
+    console.log('running background job with monitoring!');
+    
+    if(Math.random() > 0.5) {
+        throw new Error('random error');
+    } else {
+        return;
+    }
+});
 
 
 
