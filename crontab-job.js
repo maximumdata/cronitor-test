80266eb79d22e2dc9ae8d4fb758edb5a0ca95a94
Mike Dettmer <mike@mikedettmer.com>
2025-05-20 23:04:56 -0400

    lower error threshold

--- crontab-job.js
+++ crontab-job.js
@@ -1,6 +1,6 @@
 console.log('running background job with monitoring!');
 
-if(Math.random() > 0.15) {
+if(Math.random() < 0.15) {
     throw new Error('random error from crontab-job.js');
 } else {
     return;
