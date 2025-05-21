ec45725e52c0f9140abf14d38e2f056bc785ca43
Mike Dettmer <mike@mikedettmer.com>
2025-05-20 21:52:38 -0400

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const cronitor = require('cronitor')('77d933c4061d44f1b2f9ae46e6860f6c');
+
+// create an empty IIFE function
+(async () => {
+    // create a monitor
+    const monitor = await cronitor.Monitor.put({
+        type: 'job',
+        key: 'important-background-job',
+        schedule: '45 * * * *'
+    });
+
+    cronitor.wrap('important-background-job', function () {
+        console.log('running background job with monitoring!');
+        
+        if(Math.random() > 0.5) {
+            throw new Error('random error');
+        } else {
+            return;
+        }
+    });
+})();
+
+
+
+// Or, embed telemetry events directly in your code.
+// const monitor = new cronitor.Monitor('important-background-job');
+
