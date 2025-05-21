6b5675d3b6f0555d554cfd5ba1a468f00a59d386
Mike Dettmer <mike@mikedettmer.com>
2025-05-20 22:10:06 -0400

    node-cron

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const cronitor = require('cronitor')('77d933c4061d44f1b2f9ae46e6860f6c');
+const nodeCron = require('node-cron');
+cronitor.wraps(nodeCron);
 
-// create an empty IIFE function
-
-cronitor.wrap('important-background-job', function () {
+// the first parameter is now the key that Cronitor will use
+// to send telemetry events when the jobs runs, completes or fails
+cronitor.schedule('important-background-job', '*/5 * * * *', function() {
     console.log('running background job with monitoring!');
     
     if(Math.random() > 0.5) {
@@ -11,9 +13,3 @@ cronitor.wrap('important-background-job', function () {
         return;
     }
 });
-
-
-
-// Or, embed telemetry events directly in your code.
-// const monitor = new cronitor.Monitor('important-background-job');
-
